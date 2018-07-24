43f207cceb5e53a561f7df1f63cdcc12e5dbaaeb
van <van@snapzio.com>
2018-07-24 10:43:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+## Installation
+SSH:
+```
+npm install git+ssh://git@github.com:oizpans/apm-agent.git
+```
+HTTP:
+```
+npm install git+https://github.com/oizpans/apm-agent.git
+```
+
+## Usage
+Initialize apm-agent instance:
+```
+const APM = require('apm-agent');
+
+const { APM_SERVER_URL, APM_TRANSACTIONS_COUNT_PER_REQUEST, VERSION } = process.env;
+
+const apm = new APM({
+  name: 'sme/admin-web',
+  serverUrl: APM_SERVER_URL,
+  transactionsPerRequest: APM_TRANSACTIONS_COUNT_PER_REQUEST, // default: 1
+});
+```
+
+Set user context:
+```
+apm.setUserContext({ userId: '1a2b3c4d5e6f' }); // NOTE: Always set this on login and on logout.
+```
+Set tag context:
+```
+apm.setTagContext({ versionNumber: VERSION.number });
+```
+
+Set custom context:
+```
+apm.setCustomContext({ repo: 'sme/admin-web' });
+```
+
+#### Instrumentation
+Initial Page Load:
+```
+const pageLoadTransaction = apm.startTransaction('initial', 'page-load');
+
+window.addEventListener('load', () => {
+  pageLoadTransaction.end();
+});
+```
+
+With Feathers Hooks:
+```
+app.hooks({
+  before: {
+    all(context) {
+      context.params.transaction = apm.startTransaction(context.path, context.method);
+    },
+  },
+
+  after: {
+    all(context) {
+      context.params.transaction.end();
+    },
+  },
+});
+```
+
+
+#### Note
+The parameters for the ``transaction.end()`` are both optional. You can pass the result of the transaction on the first parameter (string), and the error logger (function) on the second.
+```
+transaction.end('loaded', reportError);
+```
---
+++ index.js
@@ -0,0 +1,113 @@
+const axios = require('axios');
+const uuid = require('uuidv4');
+
+const pendingTransactions = {};
+
+class Transaction {
+  constructor(name, type, app, __helpers, context) {
+    this.name = name;
+    this.type = type;
+    this.app = app;
+    this.__helpers = __helpers;
+
+    this.context = Object.assign({}, context);
+
+    if (typeof window !== 'undefined' && window && window.navigator && window.location) {
+      this.context = Object.assign({}, context, {
+        custom: Object.assign({
+          url: window.location.href,
+          userAgent: window.navigator && window.navigator.userAgent,
+        }, context.custom),
+      });
+    }
+
+    this.id = uuid();
+    this.__start = new Date();
+  }
+
+  end(result, reportError) {
+    this.result = result || this.name;
+    this.timestamp = (new Date()).toISOString();
+
+    this.duration = new Date() - this.__start;
+    const { __id, __serverUrl, __transactionsPerRequest } = this.__helpers;
+
+    delete this.__start;
+    delete this.__helpers;
+
+    pendingTransactions[__id].push(this);
+
+    if (pendingTransactions[__id].length < __transactionsPerRequest) {
+      return;
+    }
+
+    const data = {
+      service: this.app,
+      transactions: pendingTransactions[__id].map((t) => { delete t.app; return t; }),
+    };
+
+    pendingTransactions[__id].length = 0;
+
+    axios.post(__serverUrl, data, { headers: { 'Content-Type': 'application/json' } })
+      .catch((err) => {
+        pendingTransactions[__id] = [...data.transactions, ...pendingTransactions[__id]];
+        if (reportError) {
+          const newError = new Error(`Could not send transaction(s) to APM server. ${err.message}`);
+          newError.debug = [...pendingTransactions[__id]];
+          reportError(newError);
+        }
+      });
+  }
+}
+
+module.exports = class ApmAgent {
+  constructor({
+    name = 'unknown', serverUrl: __serverUrl, transactionsPerRequest: __transactionsPerRequest = 1,
+  }) {
+    if (!__serverUrl) throw new Error("'__serverUrl' is required.");
+    if (!Number(__transactionsPerRequest)) throw new Error(`'__transactionsPerRequest' must be a a number, instead got a(n) ${typeof __transactionsPerRequest} (value: ${__transactionsPerRequest}).`);
+
+    const __id = uuid();
+    pendingTransactions[__id] = [];
+
+    this.__helpers = {
+      __transactionsPerRequest,
+      __serverUrl: `${__serverUrl}/v1/client-side/transactions`,
+      __id,
+    };
+
+    this.app = {
+      name,
+      version: '0.0.1',
+      language: {
+        name: 'javascript',
+      },
+      agent: {
+        name: 'custom',
+        version: '0.0.1',
+      },
+    };
+
+    this.context = {
+      user: {},
+      tags: {},
+      custom: {},
+    };
+  }
+
+  setUserContext(userContext) {
+    this.context.user = Object.assign({}, this.context.user, userContext);
+  }
+
+  setTagContext(tagsContext) {
+    this.context.tags = Object.assign({}, this.context.tags, tagsContext);
+  }
+
+  setCustomContext(customContext) {
+    this.context.custom = Object.assign({}, this.context.custom, customContext);
+  }
+
+  startTransaction(name, type) {
+    return new Transaction(name, type, this.app, this.__helpers, this.context);
+  }
+}
